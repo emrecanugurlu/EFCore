906340797d13167c3e14ea0cca46b3195f8ed167
Emre Can UĞURLU <emrecnugurlu@gmail.com>
2023-05-14 18:56:34 +0300

    One to One
    One to One ilişkisel senaryolarda nesneyle veriyi ilişkisel veriyi güncelleme yapıldı.

---
+++ UpdatingRelatedData/Migrations/20230514131348_mig_1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpdatingRelatedData.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Addreeses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addreeses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addreeses_Persons_Id",
+                        column: x => x.Id,
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addreeses");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+        }
+    }
+}
---
+++ UpdatingRelatedData/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+ApplicationDbContext context = new ApplicationDbContext();
+#region One to One ilişkisel senaryolarda veri güncelleme
+#region Saving
+//Person person = new Person()
+//{
+//    Name = "Emre Can Uğurlu",
+//    Addrees = new Addrees() { City = "Hatay" }
+//};
+
+//Person person1 = new Person()
+//{
+//    Name = "Ahmet İlhan"
+//};
+
+//await context.AddAsync(person);
+//await context.AddAsync(person1);
+//await context.SaveChangesAsync();
+#endregion
+#region 1.Durum -> Esas tablodaki veriye bağımlı veriyi değiştirme
+Person? person = await context.Persons
+    .Include(p => p.Addrees)
+    .FirstOrDefaultAsync(p => p.Id == 1);
+
+Addrees addrees = person!.Addrees;
+context.Addreeses.Remove(addrees);
+person.Addrees = new Addrees()
+{
+    City = "Yozgat"
+};
+await context.SaveChangesAsync();
+#endregion
+#endregion
+
+
+class Person
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Addrees Addrees { get; set; }
+
+}
+class Addrees
+{
+    public int Id { get; set; }
+    public string City { get; set; }
+    public Person Person { get; set; }
+}
+
+class ApplicationDbContext : DbContext
+{
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=localhost, 1433; Database=ETrade; User Id=sa; Password=1Q2W3E4R+; TrustServerCertificate=True; Encrypt=False;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Addrees>()
+            .HasOne(a => a.Person)
+            .WithOne(p => p.Addrees)
+            .HasForeignKey<Addrees>(a => a.Id);
+    }
+    public DbSet<Addrees> Addreeses { get; set; }
+    public DbSet<Person> Persons { get; set; }
+
+
+}
