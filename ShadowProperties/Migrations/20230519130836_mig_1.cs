b387c776e0aeef63ca27af79b092ed7966cd3e2d
Emre Can UĞURLU <emrecnugurlu@gmail.com>
2023-05-19 17:11:01 +0300

    Shadow Property
    Ef Core'da shadow propertyler nedir? ve ne amaçla kullanılır?

---
+++ ShadowProperties/Migrations/20230519130836_mig_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShadowProperties.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastUpdate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_BlogId",
+                table: "Posts",
+                column: "BlogId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+        }
+    }
+}
---
+++ ShadowProperties/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+ApplicationDbContext context = new ApplicationDbContext();
+
+#region Shadow Properties
+//Entity sınıflarında fiziksel olarak tanımlanmayan/modellenmeyen fakat Ef Core tarafından ilgili entity için var olan propertilerdir.
+//Tabloda gösterilmesini istemediğimiz/lüzumlu görmediğimiz/entity intence'sı üzerinde işlem yapmayacağımız kolonlar için shadow property'ler kullılabilir.
+//Shadow propertylerin değerleri ve stateleri Change tarcker tarafından kontrol edilmektedir.
+#endregion
+
+#region Foreign Key Shadow Properties
+//İlişkisel senaryolarda foreign key propertisini tanımlamadığımız halde Ef Core tarafından depndent entity'e eklenmektedir. Buna da shadow property denilir.
+#endregion
+
+#region Shadow Property Oluturma
+//Bir entity üzerinde shadow property oluşturmak istiyorsak Fluent API'ı kullanmamız gerekmektedir. 
+//          modelBuilder.Entity<Blog>()
+//                  .Property<DateTime>("CreatedDate");
+
+#endregion
+
+
+#region Shadow property'e erişim sağlama
+
+#region Change Tracker ile erişim
+//Blog? blog = await context.Blogs.FirstAsync();
+//PropertyEntry createdDated = context.Entry(blog).Property("CreatedDate");
+//createdDated.CurrentValue = DateTime.UtcNow;
+//await context.SaveChangesAsync();
+#endregion
+
+#region EF.Property ile erişim
+
+//Blogları "CreatedDate" property'sine göre artan olarak sıralama
+var blogs = await context.Blogs.OrderBy(b => EF.Property<DateTime>(b,"CreatedDate")).ToListAsync();
+
+var blogs1 = await context.Blogs.Where(b => EF.Property<DateTime>(b, "CreatedDate").Year > 2020).ToListAsync();
+
+
+#endregion
+#endregion
+
+
+class Blog
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Post> Posts { get; set; }
+}
+
+class Post
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public Blog Blog { get; set; }
+}
+class ApplicationDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Server=localhost, 1433; Database=Application; User Id=sa; Password=1Q2W3E4R+; TrustServerCertificate=True; Encrypt=False;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Blog>()
+            .Property<DateTime>("CreatedDate");
+    }
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+}
